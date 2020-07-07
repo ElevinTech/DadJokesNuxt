733737b6576f0ab7ee8be15609323301e53f5877
Jovin Delfin <jovdelfin01@gmail.com>
2020-07-08 05:48:05 +0800

    Initial changes

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,77 @@
+
+<template>
+  <div>
+    <SearchJokes v-on:search-text="searchText" />
+      <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Joke from '../../components/Joke'
+import SearchJokes from '../../components/SearchJokes'
+
+export default {
+  component: {
+    Joke,
+    SearchJokes
+  },
+  data () {
+    return {
+      jokes: []
+    }
+  },
+  async created () {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.get('http://icanhazdadjoke.com/search', config)
+
+      this.jokes = res.data.results
+      console.log(res.data.results)
+    } catch (err) {
+      console.log('got error in jokes')
+      console.log(err)
+    }
+  },
+  methods: {
+    async searchText (text) {
+      const config = {
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config)
+
+        this.jokes = res.data.results
+        console.log(res.data.results)
+      } catch (err) {
+        console.log('got error in jokes')
+        console.log(err)
+      }
+    }
+  },
+  head () {
+    return {
+      title: 'Welcome to Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
